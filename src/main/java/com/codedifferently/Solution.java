2688301a613661deb2011685dabe4ade4ef995ea
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-07-26 09:33:50 -0400

    abdul

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,21 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public String howManyLettersDoYouSee(String input){
-        return null;
+    public String howManyLettersDoYouSee(String input) {
+        char[] chars = input.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char i : chars) {
+            if (map.containsKey(i)) {
+                int counter = map.get(i);
+                map.put(i, ++counter);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        return input;
     }
 }
